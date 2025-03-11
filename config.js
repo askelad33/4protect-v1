751b5b5b988a4ce74a2eae830fa0110e05763a38
askelad33 <mailforgta353@gmail.com>
2025-03-11 14:19:20 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
     bot: {
         prefixe: '+', // Préfixe - Le texte avant les commandes
-        buyer: '1251209545129132110', // Buyer
+        buyer: '770655207116767234', // Buyer
         couleur: '#6495ED', // Couleur HEX
-        footer: '4Protect', // Dans la commande help ou autre il y'a un texte en bas
-        maxServer: '100', // Le nombre de serveur maximum que le bot peut rejoindre 
+        footer: 'Askprotect', // Dans la commande help ou autre il y'a un texte en bas
+        maxServer: '2', // Le nombre de serveur maximum que le bot peut rejoindre 
     }
 }
